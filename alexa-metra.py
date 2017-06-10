c6e3235481c2cb1d5670e2a34e89195e70a8f30b
Phillip Nielsen <Philnielsen888@gmail.com>
2017-06-09 23:51:47 -0500

    add realtime gtfs data

--- alexa-metra.py
+++ alexa-metra.py
@@ -1,8 +1,40 @@
-from flask import Flask, render_template
-from flask_ask import Ask, statement
+import os
+import logging
+import datetime
+import math
+import re
+from six.moves.urllib.request import urlopen
+from six.moves.urllib.parse import urlencode
+from google.transit import gtfs_realtime_pb2
+from secrets import accessKey, secretKey
+import requests, zipfile, StringIO
+import transitfeed
+import os
+
+import aniso8601
+from flask import Flask, json, render_template
+from flask_ask import Ask, request, session, question, statement
+
+def update_schedule():
+    schedule_zip = requests.get('https://gtfsapi.metrarail.com/gtfs/raw/schedule.zip', auth=(accessKey, secretKey))
+    z = zipfile.ZipFile(StringIO.StringIO(schedule_zip.content))
+    z.extractall("C:\Users\Phil\PycharmProjects\\alexa-metra\schedule")
+    return
+
 
 app = Flask(__name__)
 ask = Ask(app, '/')
+
+feed = gtfs_realtime_pb2.FeedMessage()
+response = requests.get('https://gtfsapi.metrarail.com/gtfs/raw/alerts.dat', auth=(accessKey, secretKey))
+feed.ParseFromString(response.content)
+
+update_schedule()
+
+loader = transitfeed.Loader("C:\Users\Phil\PycharmProjects\\alexa-metra\schedule")
+schedule = loader.Load()
+for stop_id, stop in schedule.stops.items():
+    print stop_id
 
 @ask.intent('HelloIntent')
 def hello(firstname):
